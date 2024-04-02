96eb27b316cba5561e98f979cdab95b6d9ed0432
Taylor Stallcup <tstallcup321@gmail.com>
2024-04-02 12:33:06 -0500

    Edited state variables

--- js/main.js
+++ js/main.js
@@ -17,12 +17,23 @@ const WORD_LIST = ["table", "chair", "apple", "happy", "grape",
 
 /*----- state variables -----*/
 let board;
-let secretWord;
-let guessedLetters;
-let maxCount;
-let remainingCount;
-let inputCount;
-let finalWord;
+let secretWord; // holds random word from array
+let maxWords = 6; // number of words that can be guessed: 6 
+let inputCount = 5 // length of word/ number of letters that can be guessed: 5
+
+// * cached variables while game is ongoing
+let guessedLetters; // cached letters that were inputted
+let finalWord; // last word user inputted after max attempts
+
+//* players curr guess position below:
+let row; // height / word attempt number
+let col; // width / length of word but INDICIES
+// let remainingCount; 
+
+//* boolean to know if game is ongoing/ over
+let gameOver = false;
+
+
 
 /*----- cached elements  -----*/
 const container = document.querySelector(".container");
@@ -39,6 +50,9 @@ init();
 function init(){
  render();
  };
+
+// function to create boxes after initialized
+function createBox()
 
 function render(){
 
