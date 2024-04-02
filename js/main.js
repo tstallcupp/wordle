21cabf88c5c431ab6101c73168e06a1b5cc57457
Taylor Stallcup <tstallcup321@gmail.com>
2024-04-02 18:23:43 -0500

    Created eventlistener for keypress & function to fill the current box with the inputted letter

--- js/main.js
+++ js/main.js
@@ -26,8 +26,8 @@ let guessedLetters; // cached letters that were inputted
 let finalWord; // last word user inputted after max attempts
 
 //* players curr guess position:
-let row; // height / word attempt number
-let col; // width / length of word but INDICIES
+let row = 0; // height / word attempt number
+let col = 0; // width / length of word but INDICIES
 // let remainingCount; 
 
 //* boolean to know if game is ongoing/ over
@@ -37,26 +37,36 @@ let gameOver = false;
 
 /*----- cached elements  -----*/
 const container = document.querySelector(".container");
-let winScreen = document.querySelector(".win-screen");
-let submitBtn = document.querySelector(".submit")
+// let winScreen = document.querySelector(".win-screen");
+// let submitBtn = document.querySelector(".submit")
 
 /*----- event listeners -----*/
-
+document.addEventListener('keydown', handleKeyPress)
 //* add event listener for play again button and call render()
+
+/*----- event handlers -----*/
+function handleKeyPress(evt) {
+    if (!gameOver) {
+        const keyPress = evt.key;
+        // console.log(keyPress)
+        //* fill the box with the keypress
+        fillBox(row, col, keyPress);
+    } 
+}
 /*----- functions -----*/
 init();
 
 // Initialize all state variables, then call render()
 function init(){
     console.log('Initializing game...')
+    render();
+};
+
+
+function render(){
     // create grid in the container 
     createGrid(container);
     // createBox(container);
-    render();
- };
-
- 
- function render(){
      
 };
 
@@ -73,15 +83,23 @@ function createBox(container, row, col, letter) {
     box.id = `box${row}-${col}`
     box.textContent = letter;
     container.appendChild(box);
-    console.log(box);
+    // console.log(box);
     // return box;
 }
 
 function createGrid(container) {
     for(let r = 0; r < maxWords; r++){
         for(let c = 0; c < inputCount; c++) {
-            console.log(`Creating grid cell at row ${r}, column ${c}`);
+            // console.log(`Creating grid cell at row ${r}, column ${c}`);
             createBox(container, r, c);
         }
+    }
+}
+
+function fillBox(row, col, letter) {
+    const boxId = `#box${row}-${col}`;
+    const box = document.querySelector(boxId);
+    if (box) {
+        box.textContent = letter;
     }
 }
